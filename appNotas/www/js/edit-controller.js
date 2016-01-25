deea96d724c8c64fd4da568fe6980217354e5b34
Claudio Yacarini <cyacarini@rcp.pe>
2016-01-25 11:34:35 -0500

    states y formularios

--- appNotas/www/js/edit-controller.js
+++ appNotas/www/js/edit-controller.js
@@ -5,10 +5,27 @@ angular.module('starter.controllers')
     .controller('editCtrl', function($scope, $state, test, lista_notas){
         $scope.id_recibido = $state.params.id;
         $scope.texto_test = test;
+
+        // Funciones de búsqueda y edición
         var getNota = function(id){
             return lista_notas.filter(function(nota){
                 return nota.id === id;
             })[0]; // Siempre retorna un array, elegir el primero
         };
-        $scope.nota = getNota($scope.id_recibido);
+
+        var editarNota = function(nota){
+            for(var i=0; i<lista_notas.length; i++){
+                if(lista_notas[i].id == nota.id){
+                    lista_notas[i]=nota;
+                    return;
+                }
+            }
+        }
+
+        // copiamos la nota para no editarla en el scope
+        $scope.nota = angular.copy(getNota($scope.id_recibido));
+        $scope.guardarNota = function(){
+            editarNota($scope.nota);
+            $state.go('list');
+        };
     });
