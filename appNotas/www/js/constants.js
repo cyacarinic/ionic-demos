ccebd60b826118c267e7a8b0582c583045760940
Claudio Yacarini <cyacarini@rcp.pe>
2016-01-25 12:36:10 -0500

    local storage en app notas

--- appNotas/www/js/constants.js
+++ appNotas/www/js/constants.js
@@ -3,7 +3,8 @@
  */
 angular.module('starter.constants',[])
     .constant('test','Hello World!!')
-    .constant('lista_notas',[
+    .constant('lista_notas', angular.fromJson(window.localStorage['notas'] || '[]'));
+    /*.constant('lista_notas',[
         {id:"1", titulo:"Nota 1", descripcion:"Detalle 1"},
         {id:"2", titulo:"Nota 2", descripcion:"Detalle 2"}
-    ]);
+    ]);*/
--- appNotas/www/js/services.js
+++ appNotas/www/js/services.js
@@ -3,6 +3,11 @@
  */
 angular.module('starter.services',[])
     .factory('noteStorage', function (lista_notas) {
+        var persist = function(){
+            // localStorage solo almacena texto, entonces --> OBJECT TO JSON
+            window.localStorage['notas'] = angular.toJson(lista_notas);
+        };
+
         return {
             list: function(){
                 return lista_notas;
@@ -14,11 +19,13 @@ angular.module('starter.services',[])
             },
             create: function(nota){
                 lista_notas.push(nota);
+                persist();
             },
             update: function(nota){
                 for(var i=0; i<lista_notas.length; i++){
                     if(lista_notas[i].id == nota.id){
                         lista_notas[i]=nota;
+                        persist();
                         return;
                     }
                 }
